b60ab538173f8206088c2793341c7018adaa43f4
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-04-30 15:53:31 +0900

    コントローラの作成

--- app/controllers/formats_controller.rb
+++ app/controllers/formats_controller.rb
@@ -1,4 +1,20 @@
 class FormatsController < ApplicationController
   def index
+    @formats = Company.all
+  end
+
+  def new
+    @format = Company.new
+  end
+
+  def create
+    @format = Company.create(format_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def format_params
+    params.require(:company).permit(:company_name, :address)
   end
 end
