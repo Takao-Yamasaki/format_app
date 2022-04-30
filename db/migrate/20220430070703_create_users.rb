90f7f884d02b576b266d08d55183f4b8dd1abe62
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-04-30 16:11:17 +0900

    マイグレーションの適用

--- db/migrate/20220430070703_create_users.rb
+++ db/migrate/20220430070703_create_users.rb
@@ -1,8 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-
+      t.string :user_name, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
       t.timestamps
+      t.references :company, null: false, foreign_key: true
+      t.index :email, unique: true
     end
   end
 end
