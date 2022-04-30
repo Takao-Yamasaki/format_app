3ff650291f7c9ed4c91d99b6336e02dbcbdb5d37
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-04-30 15:48:35 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'formats/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'formats#index'
+  resouces :formats, only: [:index, :new, :create]
 end
